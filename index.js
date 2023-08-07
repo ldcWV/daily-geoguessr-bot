e125733e7411adf8b9cd700b9d5b6d83e03ad78a
sh-mug <shmug.shell@gmail.com>
2023-08-07 23:56:33 +0900

    variableize posting time as POST_TIME

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on('ready', async () => {
     const channel = await client.channels.cache.get(process.env.CHANNEL_ID);
     let inviteUrl = process.env.LAST_CHAL_URL;
 
-    cron.schedule('0 10 * * *', async () => {
+    cron.schedule(process.env.POST_TIME, async () => {
         client.login(process.env.DISCORD_TOKEN);
 
         // post challenge result
